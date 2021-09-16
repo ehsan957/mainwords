cf8fc10a5e4d8e350b7b3844622c572c9dc87b13
anvaari <anvaari@pm.me>
2021-09-16 20:23:15 +0430

    Update mainwords_fa.py
    Mainwords_fa check all_text exist and if not, create it from topblogsrss.txt.
    
    In this version, mainwords_fa remove "nim falsele" from text, tokenize them, remove signs and string contains number and finaly create dictionary of words which vlaues is number of accurance of word. Then put first 100 of them into Top100FarsiWords_py.txt

--- Farsi/mainwords_fa.py
+++ Farsi/mainwords_fa.py
@@ -16,6 +16,8 @@ import progressbar
 import pickle
 
 
+from hazm import  word_tokenize
+
 
 def get_text_from_xml(xml_content):
     bs=BeautifulSoup(xml_content,'lxml')
@@ -46,7 +48,7 @@ with open(os.path.join(script_path,'topblogsrss.txt')) as fp:
 if os.path.isfile(os.path.join(script_path,'all_text.txt')):
     with open(os.path.join(script_path,'all_text.txt')) as fp:
         all_text=fp.readlines()
-        all_text=tuple(filter(lambda x:len(x)!=0,all_text))
+        all_text=tuple(filter(lambda x:len(x)!=0,all_text)) # Remove empty strings
     
 else:
     
@@ -64,5 +66,33 @@ else:
     with open(os.path.join(script_path,'all_text.txt'),'w') as fp:
         for text in all_text:
             fp.write(text)
-
+    # Remove empty strings
     all_text=tuple(filter(lambda x:len(x)!=0,all_text))
+    
+# Create a dictionary of words
+
+words=dict()
+
+# Specify Signs and Numbers in order to avoid words contain them enter in our final Set 
+signs=['،','«','»','.',')','(','"',':',';','%','-','?',',','؛',"'",'_']
+numbers=[f'{i}' for i in range(10)]
+ 
+
+for text,i in zip(all_text,progressbar.progressbar(range(len(all_text)))):
+    text=text.replace(u'\u200c',' ') # Remove "nim fasele"
+    text_words= word_tokenize(text)
+    # Remove signs and string which contains numbers
+    text_words=tuple(filter(lambda x:x not in signs and all(tuple(map(lambda y:y not in numbers and y not in signs,x))),text_words))
+    
+    for word in text_words :
+        words[word]=words.get(word,0)+1
+    
+words=dict(sorted(words.items(),key=lambda x:x[1],reverse=True))
+
+with open(os.path.join(script_path,"Top100FarsiWords_py.txt"),'w') as fp:
+    i=0
+    for word in words:
+        fp.write(word+'\n')
+        i+=1
+        if i==100:
+            break
